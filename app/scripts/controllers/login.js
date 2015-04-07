49a0ddff88a6890d584d9bb6caac5669bcd7ead2
gerbil <jurijs.kobecs@gmail.com>
2015-04-07 09:54:10 +0300

    +

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name t2EventsApp.controller:loginCtrl
+ * @description
+ * # loginCtrl
+ * Controller of the t2EventsApp
+ */
+angular.module('t2EventsApp')
+
+    .controller('loginCtrl', function ($scope, Restangular, $location) {
+
+        // Check localStorage for apikey
+        var apikey = window.localStorage.getItem('apikey');
+
+        // Redirect to a main screen only if apikey is already set
+        if (typeof(apikey) !== 'undefined' && apikey !== null) {
+            // Redirect to a main screen
+            $location.path('main'); // path not hash
+            //console.info('Apikey found in localStorage - ' + apikey);
+        }
+
+        $scope.login = function(username, password) {
+            // Rest API communication -> send username + password, get token
+            Restangular.all('login').post({'username': username, 'password': password})
+                .then(function (result) {
+                    // Success response from the server.
+                    $scope.apikey = result.apikey;
+                    // Set localStorage for apikey
+                    window.localStorage.setItem('apikey', $scope.apikey);
+                    // Redirect to a main screen
+                    $location.path('main'); // path not hash
+                }, function() {
+                    // Error response from the server.
+                    $scope.loginForm.username.$setValidity('', false);
+                    $scope.loginForm.password.$setValidity('', false);
+                });
+        };
+
+    });
